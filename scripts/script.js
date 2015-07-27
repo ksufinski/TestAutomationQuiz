9cb0e3abd6f047104048a3fd80f9d4f1b8d0f76d
marchuk-nikolay <kolunya.marchuk@gmail.com>
2015-07-27 22:44:54 +0300

    ShowSuggestions

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,21 @@ function addInArray(element, array) {
 	array.push(element);
 }
 
+function addSuggestions(array) {
+	var trueSuggestions = [];
+	for(var questionNumber = 0; questionNumber < array.length; questionNumber += 1) {
+		if (array[questionNumber] == 'no') {
+			trueSuggestions.push(suggestions[questionNumber]);
+		}
+	}
+	if (trueSuggestions.length == 0) {
+		trueSuggestions.push("good");
+	}
+	return trueSuggestions;
+}
+
 var questions = ["aaa", "bbb", "ccc"];
+var suggestions = ["111", "222", "333"];
 var answers = [];
 var i = 0;
 
@@ -21,7 +35,13 @@ document.body.addEventListener("click", function(){
 		yes.parentNode.removeChild(yes);
 		no.parentNode.removeChild(no);
 		questionNode.parentNode.removeChild(questionNode);
-		console.log("answers", answers);
+		var result = addSuggestions(answers);
+		var resultNode = document.querySelector('.result');
+		var resultString = "";
+		for (var suggestionIndex = 0; suggestionIndex < result.length; suggestionIndex += 1) {
+			resultString += result[suggestionIndex] + "\n";
+		}
+		resultNode.innerText = resultString;
 	}
 }, false);
 
