26630e9b2865515b05dcfe3d3f92e90114fb54ea
ksufinski <ksufinski@gmail.com>
2015-07-29 04:13:16 +0300

    Have updated design and add first question

--- scripts/script.js
+++ scripts/script.js
@@ -15,19 +15,24 @@ function addSuggestions(array) {
 	return trueSuggestions;
 }
 
-var questions = ["aaa", "bbb", "ccc"];
-var suggestions = ["111", "222", "333"];
+var questions = ["All your locators are safe ? i.e. You dont have any locators lice \nxpath:.//*[@id='Z0F1D0717-7D32-452F-B127-3C9E3DDBD7D0']/div/div[4]/div/div[5]/span/span[2]/span/span", 
+
+
+"bbb", "ccc"];
+var suggestions = ["Try to use dynamic xpath, xpath functions like contains(), text()", "222", "333"];
 var answers = [];
 var i = 0;
 
 var questionNode = document.querySelector('.question');
 questionNode.innerText = questions[i];
 
-document.body.addEventListener("click", function(){
+document.body.addEventListener("click", function(e){
+	
 	var id = event.target.id;
 	var idForm = $('.form');
 	
 	if (id === "yes" || id === "no") {
+		e.preventDefault();
 		i++;
 		idForm.stop().fadeToggle();
 		$('.form').css("display", "none");
@@ -50,8 +55,18 @@ document.body.addEventListener("click", function(){
 		}
 		setTimeout(function(){
 			resultNode.innerText = resultString;
+			$('.result').css("margin-top", "300px");
 			idForm.stop().fadeToggle();			
 		}, 700);
 	}
 }, false);
+
+
+
+ $('#scrollDown ').click(function(e){
+ e.preventDefault();
+ $('body, html').animate({ scrollTop: $('#yes').offset().top }, 1000);
+ });
+
+
 
